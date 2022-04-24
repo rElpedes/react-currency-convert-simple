a290992730aaacf907881c6f6f21edff1a9bf27c
Admin <admin@WorkstationC.local>
2022-04-24 17:39:13 +0930

    Initial commit from local

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function App() {
+  return (
+    <div>
+      <h1>{new Date().toString()}</h1>
+      <p>blank webpack+react+babel template</p>
+    </div>
+  );
+}
